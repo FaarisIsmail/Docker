8cd4910052a065e045dad5a6a3d0ad0861a7bbbd
FaarisIsmail <63711786+FaarisIsmail@users.noreply.github.com>
2021-11-15 15:51:01 -0500

    added hw4

--- Project/driver/driver.py
+++ Project/driver/driver.py
@@ -1,3 +1,8 @@
+import webbrowser
+import os
+
+
+
 def main():
 
     print("Welcome to Big Data Processing Application\nPlease type the number that corresponds to which application you would like to run:\n1. Apache Hadoop\n2. Apache Spark\n3. Jupyter Notebook\n4. SonarQube and SonarScanner")
@@ -8,6 +13,8 @@ def main():
         print("Apache Spark")
     elif(val == 3):
         print("Jupyter Notebook")
+        webbrowser.open('http://localhost:8888/?token=7284adc13ac9eca5c7f8fa6ac3f09cf7a2be7d7484613609')
+        os.system("docker run -p 8888:8888 jupyter/base-notebook")
     elif(val == 4):
         print("Sonar Scanner")
     else:
